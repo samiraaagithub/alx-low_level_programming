64664a3be92ddd097d4b20211ebb0940d9cdb9e4
samiraaagithub <samira.elbechra2015@gmail.com>
2023-04-20 16:47:08 -0700

    0x00. C - Hello, World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
-#include<stoid.h>
+#include<stdio.h>
 int main(void)
 {
-puts("/"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
